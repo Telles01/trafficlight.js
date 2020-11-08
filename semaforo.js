77bab93bd9bfe5bbb9f24d414c4cf096a81ce94f
Bernard Telles <bernardtelgon@gmail.com>
2020-11-08 00:00:50 -0300

    traffic light

---
+++ semaforo.js
@@ -0,0 +1,18 @@
+const img = document.getElementById( 'img');
+const buttons = document.getElementById('buttons');
+
+const trafficLght = (event ) => {
+  turnOn[event.target.id]();
+    
+}
+
+const turnOn = {
+  'red': () => img.src = './img/vermelho.png',
+  'yellow': () => img.src = './img/amarelo.png',
+  'green': () => img.src = './img/verde.png'
+
+}
+
+
+buttons.addEventListener('click', trafficLght);
+
